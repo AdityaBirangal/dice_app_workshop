0597c6189686cdfe643f49fa262a344acb091f98
Aditya Birangal <aditya.birangal@gmail.com>
2020-04-26 17:37:59 +0530

    Body Added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,22 @@ void main() {
         appBar: AppBar(
           title: Text("DSC Dice App"),
         ),
+        body: DicePage(),
       ),
     ),
   );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("This is My First App"),
+    );
+  }
 }
